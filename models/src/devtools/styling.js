206ac263ae6434a2a24ee7efaf7e8fee82a196c0
Wired Earp <wiredearp@gmail.com>
2021-11-05 08:06:59 +0100

    Refactoring done (for now)

--- models/src/devtools/styling.js
+++
@@ -1,4 +0,0 @@
-export const simple = {};
-export const dimmed = { style: 'opacity: 0.5' };
-export const indent = { style: 'margin-left: 14px' };
-export const double = { style: 'margin-left: 28px' };
